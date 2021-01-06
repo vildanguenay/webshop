2888b14ce18c9b49676eefecf0cea094d93ab05a
Vildan Günay <vildan-guenay@hotmail.com>
2021-01-06 14:53:21 +0100

    change router paths

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import Header from "./components/Header"
-import Cart from "./pages/Cart"
-import Photos from "./pages/Photos"
+import Header from './components/Header';
+import Cart from './pages/Cart';
+import Photos from './pages/Photos';
 import './App.css';
 
 function App() {
   return (
     <div>
-      
       <Header />
       <Switch>
-        <Route exact path="/">
+        <Route exact path="/webshop">
           <Photos />
         </Route>
-        <Route exact path="/cart">
+        <Route exact path="/webshop/cart">
           <Cart />
         </Route>
       </Switch>
-
-      
     </div>
   );
 }
